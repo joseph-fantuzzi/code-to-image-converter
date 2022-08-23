01fbe291b92fc0fb8914ca9befad832a921fdf72
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-08-22 22:05:14 -0400

    styled the background for the image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+  width: 65%;
+  height: 600px;
+  margin: 0 auto;
+  border-radius: 32px;
+  background: linear-gradient(107.54deg, #ffdf70 7.1%, #980606 91.97%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const TextEditor = styled.div`
+  width: 70%;
+  height: 60%;
+  background: linear-gradient(105.19deg, rgba(0, 0, 0, 0.4725) 0%, rgba(0, 0, 0, 0.63) 101.41%);
+  box-shadow: 20px 20px 20px rgba(0, 0, 0, 0.26);
+  border-radius: 24px;
+`;
 
 const Image = () => {
-  return <div>Image</div>;
+  return (
+    <ImageContainer>
+      <TextEditor></TextEditor>
+    </ImageContainer>
+  );
 };
 
 export default Image;
