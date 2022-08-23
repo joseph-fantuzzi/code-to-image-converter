ca835e8c951f46dbe1aefe5bb7deccad4e2003d7
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-08-22 21:54:36 -0400

    styled the header and footer sections

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import "./App.css";
 import Logo from "./components/Logo";
 import Main from "./components/Main";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
+
+const InnerContainer = styled.div`
+  min-height: calc(100vh - 43.188px);
+`;
+
+const Footer = styled.footer`
+  text-align: center;
+  font-weight: 200;
+  font-size: 0.9em;
+  padding: 1em;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  padding: 3em 4em;
+`;
+
+const Header = styled.div`
+  margin-left: 4em;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  font-weight: 700;
+  margin-bottom: 0.2em;
+`;
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Logo />
-        <div>
-          <h1>Zitrous</h1>
-          <p>An all-in-one code to styled image converter.</p>
-        </div>
-      </nav>
-      <Main />
-      <footer>Designed and Created By Joseph Fantuzzi</footer>
-    </div>
+    <Container>
+      <InnerContainer>
+        <Nav>
+          <Logo />
+          <Header>
+            <Title>Zitrous</Title>
+            <p>An all-in-one code to styled image converter.</p>
+          </Header>
+        </Nav>
+        <Main />
+      </InnerContainer>
+      <Footer>
+        <p>Designed and Created By Joseph Fantuzzi</p>
+      </Footer>
+    </Container>
   );
 }
 
